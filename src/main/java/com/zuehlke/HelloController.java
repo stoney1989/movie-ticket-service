c1a6d1904361493a7c70947a2b98cc3e1d2affd5
Hartmann <Nico.Hartmann@zuehlke.com>
2017-05-10 09:29:30 +0200

    first commit

---
+++ src/main/java/com/zuehlke/HelloController.java
@@ -0,0 +1,15 @@
+package com.zuehlke;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+}
